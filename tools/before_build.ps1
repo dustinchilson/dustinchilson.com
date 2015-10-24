268c1d92e9704557e0a21146e0d99b13430afe7e
Dustin Chilson <dustinchilson@gmail.com>
2015-10-24 19:15:24 -0400

    Building

--- tools/before_build.ps1
+++ tools/before_build.ps1
@@ -1,3 +1,7 @@
 git config --global credential.helper store
 Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:github_token):x-oauth-basic@github.com`n"
+
+git config --global user.email "$($env:email)"
+git config --global user.name "Dustin Chilson"
+
 git clone https://github.com/dustinchilson/dustinchilson.github.com.git $env:site_folder 2>&1 | % { $_.ToString() }
