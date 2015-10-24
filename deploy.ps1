48059ad9973a0aff230c160cfc79755dc5814a04
Dustin Chilson <dustinchilson@gmail.com>
2015-10-24 17:53:45 -0400

    Build Work

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,5 @@
 git config --global credential.helper store
 Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:github_token):x-oauth-basic@github.com`n"
-cd $($env:site_folder)
+cd $env:site_folder
 git commit -a -m "$env:APPVEYOR_REPO_COMMIT_MESSAGE"
 git push origin master
