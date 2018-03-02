77e3aed22f570f2b54756e843ef5207569e631e1
Dustin Chilson <dustin.chilson@Salespad.com>
2018-03-02 08:13:23 -0500

    Build script update

--- _tools/before_build.ps1
+++ _tools/before_build.ps1
@@ -4,4 +4,5 @@ Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:github_token):x-
 git config --global user.email "$($env:email)"
 git config --global user.name "Dustin Chilson"
 
-git clone https://github.com/dustinchilson/dustinchilson.github.com.git $env:site_folder 2>&1 | % { $_.ToString() }
+git clone https://github.com/dustinchilson/dustinchilson.com.git $env:site_folder 2>&1 | % { $_.ToString() }
+git checkout gh-pages
