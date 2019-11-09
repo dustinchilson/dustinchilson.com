4965c5e45967a86f5937957dd289316f1c8acbaa
Dustin Chilson <dustin.chilson@salespad.com>
2019-11-08 23:03:01 -0500

    move to next.js

---
+++ blog.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMeta: {
+    title: 'Dustin Chilson',
+    author: 'Dustin Chilson',
+    image: '/static/site-feature.png',
+    description: 'Dustin Chilson, Senior Software Developer at SalesPad',
+    siteUrl: 'https://dustinchilson.com',
+    social: {
+      twitter: '_dustinchilson',
+      github: '_dustinchilson'
+    },
+    postsPerPage: 5
+  }
+}
